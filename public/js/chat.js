d1c3fcf5311f026e48948dbdb94c1605768466a8
Mustafa kunwa <m.kunwa52@gmail.com>
2020-06-01 22:19:29 +0530

    User in chat room

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,10 +10,14 @@ const $messages = document.querySelector('#messages');
 //Template
 const messageTemplate = document.querySelector('#message-template').innerHTML;
 const locationTemplate = document.querySelector('#location-template').innerHTML;
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 
 //Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
+const autoScroll = () => {
+
+}
 socket.on("message", (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
@@ -22,6 +26,7 @@ socket.on("message", (message) => {
         createdAt: moment(message.createdAt).format('h:mm a')
     });
     $messages.insertAdjacentHTML('beforeend', html);
+    autoScroll()
 })
 
 socket.on("locationMessage", (message) => {
@@ -31,6 +36,16 @@ socket.on("locationMessage", (message) => {
         createdAt: moment(message.createdAt).format('h:mm a')
     });
     $messages.insertAdjacentHTML('beforeend', html);
+    autoScroll()
+})
+
+socket.on("roomData", ({ room, users }) => {
+    console.log(room, users)
+    const html = Mustache.render(sidebarTemplate, {
+        room: room,
+        users,
+    });
+    document.querySelector('#sidebar').innerHTML = html;
 })
 
 
